4b4a3abadb508f26865175667f5f6983711f1400
Tirson Yang <2360046630@qq.com>
2025-09-11 16:56:33 +0800

    删除无用代码

--- src/test/java/Calculate.java
+++
@@ -1,9 +0,0 @@
-public class Calculate {
-
-    public static void main(String[] args) {
-        System.out.println(150*7+370+200);
-        System.out.println();
-        System.out.println(500+2300+600);
-    }
-
-}
